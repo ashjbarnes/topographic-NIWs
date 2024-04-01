625046301be22d2d947bc0fcfc66b6cd76b5a777
ashjbarnes <ashjbarnes97@gmail.com>
2024-04-01 17:45:22 +0200

    scripts for pp

---
+++ postprocess_scripts/ppscript_strength.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -N pp_strength
+#PBS -P v45
+#PBS -q normalbw
+#PBS -l mem=112gb
+#PBS -l walltime=6:00:00
+#PBS -l ncpus=28
+#PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5+scratch/nm03
+cd $PBS_O_WORKDIR
+PYTHONNOUSERSITE=1
+module use /g/data/hh5/public/modules
+module load conda/analysis3-unstable
+module list
+python3 /g/data/v45/ab8992/bottom_iwbs/repo/run_postprocessing.py -e "strength" 
