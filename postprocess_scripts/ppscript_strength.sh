fcf276032fd6bc06241da1c83e32ff0b51d23a55
ashjbarnes <ashjbarnes97@gmail.com>
2024-04-02 03:52:15 +1100

    asdf

--- postprocess_scripts/ppscript_strength.sh
+++ postprocess_scripts/ppscript_strength.sh
@@ -4,7 +4,7 @@
 #PBS -q normalbw
 #PBS -l mem=112gb
 #PBS -l walltime=6:00:00
-#PBS -l ncpus=28
+#PBS -l ncpus=56
 #PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5+scratch/nm03
 cd $PBS_O_WORKDIR
 PYTHONNOUSERSITE=1
