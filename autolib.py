264d94b75b34613dcd1f0babbe04e01d3b6b4c72
ashjbarnes <ashjbarnes97@gmail.com>
2024-08-05 15:07:03 +1000

    bf

--- autolib.py
+++ autolib.py
@@ -35,7 +35,7 @@ def setup_mom6(exptname,perturbation,overrides = [],walltime = None,default_dir 
             
         if "input" in config[line][0:10]:
             if perturbation != "common":
-                config[line] = f"input:\n     -{basepath / 'inputdir' / perturbation}\n     -{basepath / 'inputdir' / 'common'}\n"
+                config[line] = f"input:\n     -{basepath / 'inputdir' / (exptname + '_' + perturbation)}\n     -{basepath / 'inputdir' / 'common'}\n"
             else:
                 config[line] = f"input:\n     -{basepath / 'inputdir' / 'common'}\n"
         if "walltime" in config[line][0:10] and walltime != None:
