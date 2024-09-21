009c24e6fd44d84e408e517ccbbe8abc5f089bc4
ashjbarnes <ashjbarnes97@gmail.com>
2024-09-21 11:39:28 -0600

    handle run duration change

--- autolib.py
+++ autolib.py
@@ -13,7 +13,24 @@ import xrft
 basepath = Path.cwd().absolute()
 
 
-def setup_mom6(exptname,perturbation,overrides = [],walltime = None,default_dir = "default",run_duration = False):
+def overwrite_in_file(filepath,old,new):
+    file = open(filepath,"r")
+    lines = file.readlines()
+    file.close()
+    for i in range(len(lines)):
+        if old in lines[i]:
+            lines[i] = new + "\n"
+    file = open(filepath,"w")
+    file.writelines(lines)
+    file.close()
+    return
+
+
+def setup_mom6(exptname,perturbation,overrides = [],walltime = None,default_dir = "default"):
+
+    duration = "10"
+    if "duration" in exptname:
+        duration = exptname.split("_")[-1]
     ## If common forcing is provided, set another input folder that contains the windstress for all runs in this experiment
     default_dir = basepath / "rundirs" / default_dir
     perturbation = str(perturbation)
@@ -56,6 +73,7 @@ def setup_mom6(exptname,perturbation,overrides = [],walltime = None,default_dir 
     file = open(runpath / "MOM_override","w")
     file.writelines(override_file)
 
+    overwrite_in_file(f"{runpath}/input.nml","    hours = ","    hours = " + duration)
 
     
     return
